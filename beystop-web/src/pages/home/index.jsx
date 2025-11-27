91e89f8047f3071b7d1db4296fba3a13dcf5e294
itsmsarah <sarahmendesandrade1@gmail.com>
2025-11-27 18:44:30 -0300

    feat:Installed some components of react bits library and i think i finished home page

--- beystop-web/src/pages/home/index.jsx
+++ beystop-web/src/pages/home/index.jsx
@@ -1,11 +1,25 @@
 import './style.css'
+import Galaxy from '../../components/Galaxy/Galaxy'
 export const Home = () =>{
     return(
-        <body>
-            <div className="container-home">
+        <div style={{ width: '100%', height: '100vh', position: 'relative', backgroundColor:'#000000' }}>
+      {/* Fundo animado */}
+      <Galaxy 
+        mouseRepulsion={true}
+        mouseInteraction={true}
+        density={1.5}
+        glowIntensity={0.5}
+        saturation={0}
+        hueShift={240}
+      />
 
-            <h1>Seja bem-vindo</h1>
-            </div>
-        </body>
+      {/* Conteúdo sobreposto */}
+      <div  className="container-home" >
+        <h1>Seja bem-vindo</h1>
+        <div className="content-home">
+           <p>Essa página foi criada com  a finalizade de gerenciar um estacionado inspirado na maior cantora existente.Esse site foi criado a partir de uma aplicativo mas houve algumas mudanças na estrurtura</p> 
+        </div>
+      </div>
+    </div>
     )
 }
