ff2f36cad07a97b04b5a970b6a93d7190d7102c9
itsmsarah <sarahmendesandrade1@gmail.com>
2025-11-27 17:07:52 -0300

    feat:Start creating home page on pages in project

--- beystop-web/src/App.jsx
+++ beystop-web/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import { Header } from './components/header/index'
+import { Home } from './pages/home'
 
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
 
     </>
   )
---
+++ beystop-web/src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+export const Home = () =>{
+    return(
+        <body>
+            <div className="container-home">
+
+            <h1>Seja bem-vindo</h1>
+            </div>
+        </body>
+    )
+}
