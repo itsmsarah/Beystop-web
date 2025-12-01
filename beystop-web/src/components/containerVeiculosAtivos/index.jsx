69eb9cf6c6f12a6f763218a49b0de3effd6e32c1
itsmsarah <sarahmendesandrade1@gmail.com>
2025-12-01 19:54:01 -0300

    feat:Finish containerVeiculosAtivos componentization

---
+++ beystop-web/src/components/containerVeiculosAtivos/index.jsx
@@ -0,0 +1,28 @@
+import './style.css'
+
+export const VeiculosAtivosContainer = ({placa,dataEntrada,horarioEntrada,data,horario}) => {
+    return (
+        <div className="veiculos-ativos-content">
+            <h2>{placa}</h2>
+
+            <div className="informacoes-carro-container">
+                <div className="informacoes-data-content">
+                    <h3>{data}</h3>
+                    <p className='data-entrada'>{dataEntrada}</p>
+                </div>
+
+                <div className="informacoes-horario-content">
+                    <h3>{horario}</h3>
+                    <p className='horario-entrada'>{horarioEntrada}</p>
+                </div>
+            </div>
+            
+            
+
+
+            </div>
+
+
+        
+    )
+}
--- beystop-web/src/pages/veiculosativos/index.jsx
+++ beystop-web/src/pages/veiculosativos/index.jsx
@@ -1,14 +1,13 @@
-
 import FloatingLines from '../../components/FloatingLines/FloatingLines'
+import { VeiculosAtivosContainer } from '../../components/containerVeiculosAtivos'
 import './style.css'
+
 export const VeiculosAtivos = () => {
     return (
         <div style={{ width: '100%', height: '90vh', position: 'relative' }}>
             <FloatingLines
                 enabledWaves={['top', 'middle', 'bottom']}
-                // Array - specify line count per wave; Number - same count for all waves
                 lineCount={[10, 15, 20]}
-                // Array - specify line distance per wave; Number - same distance for all waves
                 lineDistance={[8, 6, 4]}
                 bendRadius={5.0}
                 bendStrength={-0.5}
@@ -18,42 +17,24 @@ export const VeiculosAtivos = () => {
 
             <div className="veiculos-ativos-container">
                 <h1>Veículos Ativos</h1>
-                <div className="veiculos-ativos-content">
-                    <h2>JGF-3348</h2>
 
-                    <div className="informacoes-carro">
-                        <div className="informacoes-data">
-                            <h3>Data/Entrada</h3>
-                            <p className='data-entrada'>10/11/2024</p>
-                        </div>
+                <VeiculosAtivosContainer
+                    placa="JGF-3348"
+                    data="Data/Entrada"
+                    horario="Horário/Entrada"
+                    dataEntrada="10/11/2024"
+                    horarioEntrada="10:29:55"
+                />
 
-                        <div className="informacoes-horario">
-                            <h3>Horário/Entrada</h3>
-                            <p className='horario-entrada'>10:29:55</p>
-                        </div>
-                    </div>
-                    <div className="veiculos-ativos-content">
-                    <h2>JGF-3348</h2>
+                <VeiculosAtivosContainer
+                    placa="ABC-1108"
+                    data="Data/Entrada"
+                    horario="Horário/Entrada"
+                    dataEntrada="11/10/2025"
+                    horarioEntrada="00:30:20"
+                />
 
-                    <div className="informacoes-carro">
-                        <div className="informacoes-data">
-                            <h3>Data/Entrada</h3>
-                            <p className='data-entrada'>10/11/2024</p>
-                        </div>
-
-                        <div className="informacoes-horario">
-                            <h3>Horário/Entrada</h3>
-                            <p className='horario-entrada'>10:29:55</p>
-                        </div>
-                    </div>
-
-                    
-                </div>
-
-                    
-                </div>
             </div>
         </div>
-
     )
 }
