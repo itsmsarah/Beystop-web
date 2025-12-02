8adb091b331d40bdd9d4445a88e4f7d9e0510378
itsmsarah <sarahmendesandrade1@gmail.com>
2025-12-01 21:14:14 -0300

    feat:Create menu hamburguer in header component

--- beystop-web/src/components/header/index.jsx
+++ beystop-web/src/components/header/index.jsx
@@ -1,12 +1,23 @@
 
 import './style.css'
 import Logo from "../../assets/images/logo.svg"
+import { useState } from 'react';
 
 export const Header = () =>{
+     const [menuOpen, setMenuOpen] = useState(false);
     return(
         <header>
             <img src={Logo} alt="imagem da beyonce" />
-            <nav className='nav-header'>
+
+             <div 
+                className={`hamburger ${menuOpen ? "open" : ""}`}
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav className={`nav-header ${menuOpen ? "active" : ""}`}>
                 <ul>
                     <li>Veículos Ativos</li>
                     <li>Liberação</li>
@@ -15,7 +26,7 @@ export const Header = () =>{
             </nav>
 
            
-            <div className="btns-header">
+            <div className={`btns-header ${menuOpen ? "active" : ""}`}>
                 <a className="btn-login"href="">Login</a>
                 <a className="btn-cadastro" href="">Cadastra-se</a>
             </div>
